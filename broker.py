6e0c229e67896b3d62b48b74d6b277a15965bf86
Travis Cammack <cammack@utexas.edu>
2020-11-10 23:03:09 -0600

    - added on to position list with days held and fancy grid

--- broker.py
+++ broker.py
@@ -5,11 +5,14 @@ from enum import Enum
 from httpx import codes
 from typing import Collection, List
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Position(BaseModel):
     name: str
     size: int
+    cost_basis: float
+    time_acquired: datetime
 
     def __hash__(self):
         return hash(self.name)
@@ -44,16 +47,72 @@ class Order(BaseModel):
     avg_fill_price: float
 
 
+class Quote(BaseModel):
+    name: str
+    price: float
+
+
+class AccountBalance:
+
+    def __init__(
+            self,
+            total_cash: float,
+            total_equity: float,
+            open_pl: float,
+            settled_cash: float):
+        self._total_cash = total_cash
+        self._total_equity = total_equity
+        self._open_pl = open_pl
+        self._settled_cash = settled_cash
+
+    @property
+    def total_cash(self):
+        return self._total_cash
+    
+    @property
+    def total_equity(self):
+        return self._total_equity
+    
+    @property
+    def open_pl(self):
+        return self._open_pl
+
+    @property
+    def settled_cash(self):
+        return self._settled_cash
+
+
 class Broker(ABC):
 
     def __init__(self, account_number: str):
         self._account_number = account_number
 
+    @abstractmethod
+    async def place_market_sell(self, position: Position):
+        pass
+
+    @abstractmethod
+    async def place_market_buy(self, position: Position):
+        pass
+    
     @property
     @abstractmethod
     async def positions(self) -> List[Position]:
         pass
+    
+    @abstractmethod
+    async def get_quote(self, name: str) -> Quote:
+        pass
 
+    @abstractmethod
+    async def get_quotes(self, names: Collection[str]) -> List[Quote]:
+        pass
+
+    @property
+    @abstractmethod
+    async def account_balance(self) -> AccountBalance:
+        pass
+ 
 
 class Tradier(Broker):
 
@@ -92,6 +151,62 @@ class Tradier(Broker):
 
         return complete_url
 
+    async def _place_order(self, position: Position, side: str):
+    
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url=self._form_url('/accounts/[[account]]/orders'),
+                data={
+                    'class': 'equity',
+                    'symbol': position.name,
+                    'side': side,
+                    'quantity': position.size,
+                    'type': 'market',
+                    'duration': 'day'
+                },
+                headers=self._headers
+            )
+    
+        if response.status_code != codes.OK:
+            raise IOError(
+                f'failed to place market {side} order from Tradier '
+                f'for {position.name} with a status code of'
+                f' {response.status_code}: {response.text}'
+            )
+    
+        return response.json()['order']['id']
+
+    async def place_market_sell(self, position: Position):
+        return await self._place_order(position, 'sell')
+
+    async def place_market_buy(self, position: Position):
+        return await self._place_order(position, 'buy')
+    
+    @property
+    def account_balance(self) -> AccountBalance:
+    
+        with httpx.Client() as client:
+            response = client.get(
+                url=self._form_url('/accounts/[[account]]/balances/'),
+                headers=self._headers
+            )
+    
+        if response.status_code != httpx.codes.OK:
+            raise IOError(
+                f'failed to get account balance for account '
+                f'{self._account_number} with a status code of '
+                f'{response.status_code}: {response.text}'
+            )
+    
+        balances = response.json()['balances']
+    
+        return AccountBalance(
+            total_cash=balances['total_cash'],
+            total_equity=balances['total_equity'],
+            open_pl=balances['open_pl'],
+            settled_cash=balances['cash']['cash_available']
+        )
+    
     @property
     def positions(self) -> List[Position]:
 
@@ -116,13 +231,73 @@ class Tradier(Broker):
         if isinstance(positions, list):
 
             return [
-                Position(name=pos['symbol'], size=pos['quantity'])
-                for pos in positions
+                Position(
+                    name=pos['symbol'],
+                    size=pos['quantity'],
+                    cost_basis=pos['cost_basis'],
+                    time_acquired=datetime.strptime(
+                        pos['date_acquired'],
+                        '%Y-%m-%dT%H:%M:%S.%fZ'
+                    )
+                ) for pos in positions
             ]
 
         else:
 
-            return [Position(
-                name=positions['symbol'],
-                size=positions['quantity']
-            )]
+            return [
+                Position(
+                    name=positions['symbol'],
+                    size=positions['quantity'],
+                    cost_basis=positions['cost_basis'],
+                    time_acquired=datetime.strptime(
+                        positions['date_acquired'],
+                        '%Y-%m-%dT%H:%M:%S.%fZ'
+                    )
+                )
+            ]
+
+    def get_quote(self, name: str) -> Quote:
+    
+        with httpx.Client() as client:
+            response = client.get(
+                url=self._form_url('/markets/quotes/'),
+                params=dict(symbols=name, greeks=False),
+                headers = self._headers
+            )
+    
+        if response.status_code != codes.OK:
+            raise IOError(
+                f'failed to get quote from Tradier for symbol(s) {name} '
+                f'with a status code of {response.status_code}: {response.text}'
+            )
+    
+        quote_object = response.json()['quotes']['quote']
+    
+        return Quote(
+            name=quote_object['symbol'],
+            price=float(quote_object['last'])
+        )
+
+    def get_quotes(self, names: Collection[str]) -> List[Quote]:
+    
+        with httpx.Client() as client:
+            response = client.get(
+                url=self._form_url('/markets/quotes/'),
+                params=dict(symbols=','.join(names), greeks=False),
+                headers=self._headers
+            )
+    
+        if response.status_code != codes.OK:
+            raise IOError(
+                f'failed to get quotes from Tradier for symbols {names} '
+                f'with a status code of {response.status_code}: {response.text}'
+            )
+    
+        quote_objects = response.json()['quotes']['quote']
+    
+        return [
+            Quote(
+                name=quote_object['symbol'],
+                price=float(quote_object['last'])
+            ) for quote_object in quote_objects
+        ]
--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import click
-import httpx
+import asyncio
+import pydantic
+
+from functools import wraps
+from tabulate import tabulate
+from datetime import datetime
 
 import broker as br
+
+
+def coro(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+    return wrapper
+
+
+class DetailedPosition(pydantic.BaseModel):
+    name: str
+    cost_basis: float
+    quantity: int
+    current_basis: float
+    allocation: float
+    gain_loss: float
+    gain_loss_percent: float
+
+
+def red(text):
+    return f'\033[91m{text}\033[0m'
+
+
+def green(text):
+    return f'\033[32m{text}\033[0m'
 
 
 @click.group()
@@ -11,9 +41,17 @@ def cli():
 
 @cli.group()
 def position():
-    click.echo('position')
+    pass
 
- 
+
+def percent_change(start, end):
+    return ((end - start) / start) * 100
+
+
+def color_pl(pl):
+    return green(pl) if pl >= 0 else red(pl)
+
+
 @position.command()
 def list():
 
@@ -24,8 +62,72 @@ def list():
     
     positions = broker.positions
     
-    for pos in positions:
-        click.echo(pos)
+    if len(positions) > 1:
+        quotes = broker.get_quotes([x.name for x in positions])
+    else:
+        quotes = broker.get_quote(positions[0].name)
+        
+    account = broker.account_balance
+    now = datetime.utcnow()
+    
+    table = [
+        [
+            x.name,
+            x.size,
+            color_pl(percent_change(x.cost_basis, y.price * x.size)),
+            ((y.price * x.size) / account.total_equity) * 100,
+            (now - x.time_acquired).days
+        ] for x, y in zip(
+            sorted(positions, key=lambda x: x.name),
+            sorted(quotes, key=lambda x: x.name))
+        ]
+    #  TODO: add in days in position
+    click.echo()
+    click.echo(
+        tabulate(
+            table,
+            headers=[
+                f'Name ({len(table)})',
+                'Quantity',
+                'Gain/Loss (%)',
+                'Concentration (%)',
+                'Days Held'
+            ],
+            tablefmt='fancy_grid',
+            floatfmt='.2f'
+        )
+    )
+    click.echo()
+    
+    
+@position.command()
+@click.argument('name')
+@click.option('-a', '--allocation', type=click.IntRange(1, 100), default=2)
+@click.option('-s', '--stop-loss', type=click.IntRange(1, 50), default=None)
+@click.option('-t', '--take-profit', type=click.IntRange(1, 1000), default=None)
+@click.option('-p', '--preview/--no-preview', default=True)
+def enter(name, allocation, stop_loss, take_profit, preview):
+    
+    broker = br.Tradier(
+        '6YA05267',
+        access_token='ey39F8VMeFvhNsq4vavzeQXThcpL'
+    )
+    
+    balances = broker.account_balance
+    account_base = balances.total_equity - balances.open_pl
+    
+    allocation_size = (allocation / 100) * account_base
+    
+    #quote = broker.get_quote(name)
+    
+
+
+@position.command()
+@click.argument('name')
+def exit(name):
+    #  TODO: check that name is actually currently held
+    #  TODO: remove any open orders
+    click.echo(f'exit a position {name}')
 
 
 if __name__ == '__main__':
